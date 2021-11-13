3408f1885c37511b087d18abcf7c9232bffe0717
avera6756 <avera6756@utm.edu.ec>
2021-11-13 15:41:28 -0500

    use state

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import BtAddToCart from '@icons/bt_add_to_cart.svg';
 
@@ -6,6 +6,12 @@ import '../styles/ProductItem.scss';
 
 
 const ProductItem = () => {
+	const [cart, setCart] = useState(false);
+
+	const addToCartHandler = () => {
+		setCart(!cart);
+	}
+
     return (
         <div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -14,8 +20,8 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
-					<img src={BtAddToCart} alt="Add to cart" />
+				<figure onClick ={addToCartHandler}>
+					<img src={!cart ? BtAddToCart : "Added To Cart"} alt="Add to cart" />
 				</figure>
 			</div>
 		</div>
